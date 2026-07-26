393684e0093d3394883096d1f44c00f4cad73938
TallishLake5581 <134895708+TallishLake5581@users.noreply.github.com>
2026-07-26 15:37:26 +0400

    server.js

--- server.js
+++ server.js
@@ -4,6 +4,13 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// رد خاص على طلب فحص الإصدار
+app.get('/persist/version.txt', (req, res) => {
+    console.log('[CardWars] Version check requested!');
+    res.status(200).send("1.0.0");
+});
+
+// الرد بـ 200 على أي طلب آخر
 app.all('*', (req, res) => {
     console.log(`[CardWars Request] Method: ${req.method} | Path: ${req.url}`);
     res.status(200).send("OK");
