12522b0e53c8c892e18defac77dc4ac885095f48
TallishLake5581 <134895708+TallishLake5581@users.noreply.github.com>
2026-07-27 03:02:10 +0400

    server.js

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
-// السماح بجميع الطلبات وتجنب خطأ 400
+// 1. طباعة كل الطلبات القادمة من اللعبة لمعرفتها
 app.use((req, res, next) => {
     console.log("طلب قادم من اللعبة:", req.method, req.url);
     next();
 });
 
-// مسار شامل للبطولات (Deck Wars) لتأكيد الاتصال
+// 2. مسار خاص لملف manifest.json لحل طلبات الـ ETag والتحديثات
+app.get('/manifest.json', (req, res) => {
+    res.status(200).json({});
+});
+
+// 3. مسار شامل لكل طلبات البطولات والـ CDN والخدمات الأخرى
 app.all('*', (req, res) => {
     res.status(200).send({
         status: "ok",
         message: "Deck Wars Server Connected"
     });
-    
